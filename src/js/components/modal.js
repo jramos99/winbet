7fa6a68e2e0dde84feee3647fccc4994a1ee770b
jramos99 <116893620+jramos99@users.noreply.github.com>
2023-03-13 14:01:08 -0600

    agrego la funcionalidad del modal

---
+++ src/js/components/modal.js
@@ -0,0 +1,22 @@
+const modal = document.getElementById("modalPromos");
+const close = document.getElementById("closeModalPromos");
+
+function closeModal() {
+    // localStorage.removeItem("modalAbierto");
+    if(localStorage.getItem("modalAbierto")!="false"){
+        modal.classList.remove("hide");
+    }
+
+    close.addEventListener("click",(e)=>{
+        e.preventDefault();
+        modal.classList.add("hide");
+        localStorage.setItem("modalAbierto", false);
+    });
+    window.addEventListener("load",function(){
+        console.log("pagina cargada")
+    })
+}
+
+export{
+    closeModal,
+}
--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,15 @@ import {tnsSingleGames} from './components/tns-slider';
 import {topNav} from './components/topNav';
 import {bottomNav} from './components/bottomNav';
 import {accordionCard} from './components/accordion';
+import { closeModal } from './components/modal';
 
 (()=>{
 	topNav();
 	bottomNav();
+	
 	if (document.body.classList.contains('inicio')) {	
 		// functions here
-		
+		closeModal();
 		tnsSingleGames();
 	}else if (document.body.classList.contains('reglas')) {
 		accordionCard();
